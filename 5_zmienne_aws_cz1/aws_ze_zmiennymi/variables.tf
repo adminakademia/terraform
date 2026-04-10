836324b80e42a51eff4614b62c93c864bbd5e05d
adminakademia <104914726+adminakademia@users.noreply.github.com>
2026-04-10 22:34:22 +0200

    .

---
+++ 5_zmienne_aws_cz1/aws_ze_zmiennymi/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+  description = "Klucz API - AWS Access Key"
+  type = string
+  sensitive = true
+}
+
+variable "aws_secret_key" {
+  description = "Klucz API - AWS Secret Key"
+  type = string
+  sensitive = true
+}
