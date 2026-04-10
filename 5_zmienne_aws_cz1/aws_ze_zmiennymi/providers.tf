179c7417578e60742e61d75f93218d9b2a4dfd61
adminakademia <104914726+adminakademia@users.noreply.github.com>
2026-04-10 22:32:53 +0200

    .

---
+++ 5_zmienne_aws_cz1/aws_ze_zmiennymi/providers.tf
@@ -0,0 +1,13 @@
+# Configure the AWS Provider
+provider "aws" {
+  region     = "eu-north-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+provider "aws" {
+  alias      = "eu-central-1"
+  region     = "eu-central-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
