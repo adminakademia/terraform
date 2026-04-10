497a24d68ebaed4b274caee65f25e5dc3746997a
adminakademia <104914726+adminakademia@users.noreply.github.com>
2026-04-10 22:34:03 +0200

    .

---
+++ 5_zmienne_aws_cz1/aws_ze_zmiennymi/klucz-ssh.tf
@@ -0,0 +1,10 @@
+resource "aws_key_pair" "jaroslaw_aws_ec2" {
+  key_name   = "jaroslaw-aws-ec2"
+  public_key = file("${path.module}/testowyklucz.pub")
+}
+
+resource "aws_key_pair" "jaroslaw_aws_ec2_eu-central-1" {
+  provider   = aws.eu-central-1
+  key_name   = "jaroslaw-aws-ec2"
+  public_key = file("${path.module}/testowyklucz.pub")
+}
